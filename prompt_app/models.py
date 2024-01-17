aea9f09b61c750ef9d613a7481cf520e65ef9423
Meron <merronmuche@gmail.com>
2024-01-17 22:41:17 +0300

    add a document model

--- prompt_app/models.py
+++ prompt_app/models.py
@@ -1,3 +1,5 @@
+
+
 from django.db import models
 
 class Prompt(models.Model):
@@ -7,3 +9,14 @@ class Prompt(models.Model):
 
     def __str__(self):
         return f"Prompt: {self.input_text[:50]}..."  # Display first 50 characters
+    
+
+class Document(models.Model):
+    pdf_file = models.FileField(upload_to='pdfs/')
+    chunks = models.JSONField(blank=True, null=True) 
+    embeds = models.JSONField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"PDF File uploaded on {self.created_at}"
